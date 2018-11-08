5fc6d1ee7b82c3f2ba631c3eefa52bc522a47375
DanielJBailey <Dbailey0210@gmail.com>
2018-11-08 13:44:37 -0700

    initial commit

---
+++ eight_ball.rb
@@ -0,0 +1,30 @@
+# Magic Eight Ball Assignment Pseudocode:
+# The team leader: in the command line:
+
+# Create an eight_ball directory
+# Create main.rb and answers.rb files in that directory
+# Create a git repository in that directory
+# The team leader then create a git repository on GitHub, push project to GitHub, and add additional partners to project.
+
+# Divide up the work below to complete the project please try to focus on using Classes for this assignment. (Don’t be afraid of more than one person coding the same file, as this will give you practice with Git flow and merge conflict resolution. The more practice you have now with Git the better you will be down the road.)
+
+ 
+
+# Basic Objectives:
+
+# - User inputs question
+# - Computer outputs random answer
+# - User inputs "QUIT"
+# - Computer outputs a goodbye message and exits
+# - ability to add more answers:
+# - via easter egg question ("add_answers")
+# - do not let them add the same answer if the eight ball already has that answer
+# - ability to reset answers back to the original bank (hint: think arr.clone)
+# - via easter egg question ("reset_answers")
+# - ability to have eight ball print all answers
+# - via easter egg question ("print_answers")
+# Bonus Objectives:
+
+# -ability to use script arguments when the magic eight ball is started to do bonus functionality above
+# - for example ruby magic_eight.RB add_answers
+# this would start your script but instead of running the regular way you would be prompted to add answers first
