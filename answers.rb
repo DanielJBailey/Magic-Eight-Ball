c408f064a1009cebea1bb17c2d7065f8d1303e1f
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-08 14:11:33 -0700

    add class answers to answers.rb

--- answers.rb
+++ answers.rb
@@ -1,3 +1,12 @@
-def method
-    puts "test"
-end
+class Answers 
+  @answers = [
+    "Utah Jazz", 
+    "You should see a doctor", 
+    "Ask your mother",
+    "Outcome looks bleak" 
+    "How dare you"
+    "YoU DoN't sAy ThAt"
+    "11"
+    "Yes, no, maybe so"
+  ]
+end # this ends the class
